f2a40cc1ac56855a40c5e20f3a2d054ec10f47e2
Girish Parate <girishparate12@gmail.com>
2022-05-18 15:19:50 +0530

    added changes for displaying data from backend for answer

--- bot/consumers.py
+++ bot/consumers.py
@@ -1,17 +1,16 @@
 # chat/consumers.py
-from ast import keyword
 import json
-from urllib import request
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import User
-
-from bot.models import Question
+import re
+from bot.models import Answer, Question
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = 'chatbot'
+        self.user = self.scope["user"]
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
@@ -24,7 +23,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': "Hello, user"
+                'message': "Hello, "+str(self.user)
             }
         )
         
@@ -39,13 +38,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
+        
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        if self.scope['user'].username:
-            # await self.save_question(message)
-            await self.answer_question(message)
-        else:
-            pass
         # Send message to room group
         await(self.channel_layer.group_send)(
             self.room_group_name,
@@ -55,9 +50,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
+        if self.scope['user'].username:
+            # await self.save_question(message)
+            await self.answer_question(message)
+        else:
+            pass
+        
     # Receive message from room group
     async def chat_message(self, event):
-        print("chat_message")
         message = event['message']
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
@@ -70,17 +70,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
         question.save()
         return 0
     
-
-    async def answer_question(self, message):
-        exclude_words = ['when', 'what', 'where', 'why', 'how', 'if']
+    @database_sync_to_async
+    def answer_question(self, message):
+        
+        exclude_words = ['when', 'what', 'where', 'why', 'how', 'if', 'is']
         kwords = [i.lower() for i in message.split() if i.lower() not in exclude_words]
-        question = Question.objects.filter(keyword__contains=message)
-        event = {'message': '<html>\n hii</html>'}
+        strn = '<div>'
+        for i in kwords:
+            datas = re.sub(r'[^\w]', '', i)
+            questiond = Question.objects.filter(keyword__contains=datas)
+            for ique in questiond:
+                data_anw = Answer.objects.filter(question=ique).values('answer', 'id')
+                for anse in data_anw:
+                    strn += '<div id='+str(anse['id'])+'>'+str(anse['answer'])+'</div><br>'
+        
+        strn += '</div>'
+        event = {'message': strn}
         text_data_json = json.loads(json.dumps(event))
         message = text_data_json['message']
-
-        # Send message to room group
-        await(self.channel_layer.group_send)( self.room_group_name,
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message
